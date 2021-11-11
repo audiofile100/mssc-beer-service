709ada519c3045aca25c2c2b2695ab7204101cae
audiofile100 <amplified100@gmail.com>
2021-11-11 12:53:44 -0700

    model

---
+++ src/main/java/com/audiofile/model/BeerStyleEnum.java
@@ -0,0 +1,15 @@
+package com.audiofile.model;
+
+public enum BeerStyleEnum {
+
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
